b091ae48326f037d50330ef68a4e4cd4e4e51dac
brandonlopes <brandonnlopes@protonmail.com>
2020-09-10 11:28:17 -0400

    drawing questions/answers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 let root = document.documentElement;
+let numberOfQuestions = 10;
+let categoryID = "";
+let triviaDifficulty = "";
+
+testData = [
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Where is the train station &quot;Llanfair&shy;pwllgwyngyll&shy;gogery&shy;chwyrn&shy;drobwll&shy;llan&shy;tysilio&shy;gogo&shy;goch&quot;?",
+        "correct_answer": "Wales",
+        "incorrect_answers": [
+            "Moldova",
+            "Czech Republic",
+            "Denmark"
+        ]
+    },
+];
+
 
 document.body.onload = () => {
     drawCategories();
-
-    let numberOfQuestions = 10;
-    let categoryIndex = 10;
-    let difficulty = "easy";
-    let quizType = "multiple" // multiple or boolean
-    let apiString = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${categoryIndex}&difficulty=${difficulty}&type=${quizType}`
-
-    queryTriviaDB(apiString);
 }
 
 
@@ -27,14 +38,29 @@ function getAnswers(triviaObjects) {
     });
 }
 
-function makeQuiz(triviaObjects) {
+function makeQuiz(triviaData) {
+    let potentialAnswers = "";
+    triviaData.forEach((question, index) => {
+        potentialAnswers = question.incorrect_answers.concat(question.correct_answer);
+        potentialAnswers = shuffleAnswers(potentialAnswers);
+        triviaData[index].potentialAnswers = potentialAnswers;
+    });
+    // triviaData.potentialAnswers = potentialAnswers;
+    drawQuiz(triviaData);
+}
 
+function shuffleAnswers(answers) {
+    for (let i = answers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+    return answers;
 }
 
 function queryTriviaDB(url) {
     fetch(url).then(function (response) {
         response.json().then(function (data) {
-            getAnswers(data.results);
+            makeQuiz(data.results);
         });
     });
 }
@@ -71,27 +97,63 @@ function drawCategories() {
     let categoryDiv = document.getElementById("categories");
 
     categories.forEach(category => {
-        let item = document.createElement("button");
-        item.addEventListener("click", () => {
-            console.log(categories.indexOf(item.textContent));
-            clearElement(categoryDiv.id);
+        let button = document.createElement("button");
+
+        button.addEventListener("click", () => {
+            categoryID = categories.indexOf(button.textContent) + 9;
+            categoryDiv.innerText = "";
             drawDifficultyScreen();
         });
-        item.textContent = category;
-        categoryDiv.appendChild(item);
+
+        button.textContent = category;
+        categoryDiv.appendChild(button);
     });
 }
 
-function drawDifficultyScreen(){
+function drawDifficultyScreen() {
     const difficulties = ["easy", "medium", "hard"];
+    let categories = document.getElementById("categories");
+
     difficulties.forEach(difficulty => {
         let button = document.createElement("button");
-        button.addEventListener("click", () => {
+        button.textContent = difficulty;
 
-        })
+        button.addEventListener("click", () => {
+            triviaDifficulty = difficulties.find(element => difficulty === button.textContent);
+            categories.remove();
+            makeQuiz(testData);
+        });
+
+        categories.appendChild(button);
     });
 }
 
-function clearElement(element) {
-    document.getElementById(element).innerText = "";
+function drawQuiz(triviaData) {
+    let apiString = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${categoryID}&difficulty=${triviaDifficulty}`;
+    // queryTriviaDB(apiString);
+    let question = document.createElement("p");
+    console.log(triviaData[0].question);
+    
+    question.innerHTML = triviaData[0].question;
+    let content = document.getElementById("content")
+    content.appendChild(question);
+    
+    triviaData[0].potentialAnswers.forEach(answer => {
+        console.log(answer);
+        
+        let answers = document.createElement("input");
+        let label = document.createElement("label");
+        answers.type = "radio"
+        answers.name = "answers"
+        answers.id = answer;
+        label.htmlFor = answer;
+        label.appendChild(document.createTextNode(answer));
+
+        let answerDiv = document.createElement("div");
+        answerDiv.appendChild(answers);
+        answerDiv.appendChild(label);
+        content.appendChild(answerDiv);
+    });
+    
+    
 }
--- js/trivia_data.js
+++ js/trivia_data.js
@@ -1,36 +1,135 @@
 let categories = [
-    "General Knowledge",
-    "Entertainment: Books",
-    "Entertainment: Film",
-    "Entertainment: Music",
-    "Entertainment: Musicals & Theatres",
-    "Entertainment: Television",
-    "Entertainment: Video Games",
-    "Entertainment: Board Games",
-    "Science & Nature",
-    "Science: Computers",
-    "Science: Mathematics",
-    "Mythology",
-    "Sports",
-    "Geography",
-    "History",
-    "Politics",
-    "Art",
-    "Celebrities",
-    "Animals",
-    "Vehicles",
-    "Entertainment: Comics",
-    "Science: Gadgets",
-    "Entertainment: Japanese Anime & Manga",
-    "Entertainment: Cartoon & Animations"
+  {
+    "id": 9,
+    "name": "General Knowledge"
+  },
+  {
+    "id": 10,
+    "name": "Entertainment: Books"
+  },
+  {
+    "id": 11,
+    "name": "Entertainment: Film"
+  },
+  {
+    "id": 12,
+    "name": "Entertainment: Music"
+  },
+  {
+    "id": 13,
+    "name": "Entertainment: Musicals & Theatres"
+  },
+  {
+    "id": 14,
+    "name": "Entertainment: Television"
+  },
+  {
+    "id": 15,
+    "name": "Entertainment: Video Games"
+  },
+  {
+    "id": 16,
+    "name": "Entertainment: Board Games"
+  },
+  {
+    "id": 17,
+    "name": "Science & Nature"
+  },
+  {
+    "id": 18,
+    "name": "Science: Computers"
+  },
+  {
+    "id": 19,
+    "name": "Science: Mathematics"
+  },
+  {
+    "id": 20,
+    "name": "Mythology"
+  },
+  {
+    "id": 21,
+    "name": "Sports"
+  },
+  {
+    "id": 22,
+    "name": "Geography"
+  },
+  {
+    "id": 23,
+    "name": "History"
+  },
+  {
+    "id": 24,
+    "name": "Politics"
+  },
+  {
+    "id": 25,
+    "name": "Art"
+  },
+  {
+    "id": 26,
+    "name": "Celebrities"
+  },
+  {
+    "id": 27,
+    "name": "Animals"
+  },
+  {
+    "id": 28,
+    "name": "Vehicles"
+  },
+  {
+    "id": 29,
+    "name": "Entertainment: Comics"
+  },
+  {
+    "id": 30,
+    "name": "Science: Gadgets"
+  },
+  {
+    "id": 31,
+    "name": "Entertainment: Japanese Anime & Manga"
+  },
+  {
+    "id": 32,
+    "name": "Entertainment: Cartoon & Animations"
+  }
+]
+
+let test = [
+  "General Knowledge",
+  "Entertainment: Books",
+  "Entertainment: Film",
+  "Entertainment: Music",
+  "Entertainment: Musicals & Theatres",
+  "Entertainment: Television",
+  "Entertainment: Video Games",
+  "Entertainment: Board Games",
+  "Science & Nature",
+  "Science: Computers",
+  "Science: Mathematics",
+  "Mythology",
+  "Sports",
+  "Geography",
+  "History",
+  "Politics",
+  "Art",
+  "Celebrities",
+  "Animals",
+  "Vehicles",
+  "Entertainment: Comics",
+  "Science: Gadgets",
+  "Entertainment: Japanese Anime & Manga",
+  "Entertainment: Cartoon & Animations"
 ];
 
 let entertainment = [];
 let science = [];
 
 for (let i = 0; i < categories.length; i++) {
-    if (categories[i].includes("Entertainment")) { categories[i] = categories[i].replace("Entertainment:", " ") }
-    if (categories[i].includes("Science")) { categories[i] = categories[i].replace("Science:", " ") }
+  if (categories[i].name.includes("Entertainment")) { categories[i] = categories[i].replace("Entertainment:", " ") }
+  if (categories[i].name.includes("Science")) { categories[i] = categories[i].replace("Science:", " ") }
 
 }
 
@@ -39,14 +138,14 @@ console.log(categories);
 let subCategories = entertainment.concat(science);
 
 let exampleQuestion = {
-    "category": "Entertainment: Books",
-    "type": "multiple",
-    "difficulty": "easy",
-    "question": "George Orwell wrote this book, which is often considered a statement on government oversight.",
-    "correct_answer": "1984",
-    "incorrect_answers": [
-      "The Old Man and the Sea",
-      "Catcher and the Rye",
-      "To Kill a Mockingbird"
-    ]
-  }
+  "category": "Entertainment: Books",
+  "type": "multiple",
+  "difficulty": "easy",
+  "question": "George Orwell wrote this book, which is often considered a statement on government oversight.",
+  "correct_answer": "1984",
+  "incorrect_answers": [
+    "The Old Man and the Sea",
+    "Catcher and the Rye",
+    "To Kill a Mockingbird"
+  ]
+}
