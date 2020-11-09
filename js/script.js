a5bad4b90dab433b7d95274fb65ffa85cb539dc8
brandonlopes <49502102+brandonlopes@users.noreply.github.com>
2020-11-09 15:54:40 -0500

    Async/Await function implemented

--- js/script.js
+++ js/script.js
@@ -1,125 +1,6 @@
 
 // let root = document.documentElement;
 
-testData = [
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "medium",
-        "question": "What is the defining characteristic of someone who is described as hirsute?",
-        "correct_answer": "Hairy",
-        "incorrect_answers": [
-            "Rude",
-            "Funny",
-            "Tall"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "medium",
-        "question": "Which language is NOT Indo-European?",
-        "correct_answer": "Hungarian",
-        "incorrect_answers": [
-            "Russian",
-            "Greek",
-            "Latvian"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "medium",
-        "question": "What is the unit of currency in Laos?",
-        "correct_answer": "Kip",
-        "incorrect_answers": [
-            "Ruble",
-            "Konra",
-            "Dollar"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "medium",
-        "question": "Which of these is the name of a Japanese system of alternative medicine, literally meaning &quot;finger pressure&quot;?",
-        "correct_answer": "Shiatsu",
-        "incorrect_answers": [
-            "Ukiyo",
-            "Majime",
-            "Ikigai"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "On average, at least 1 person is killed by a drunk driver in the United States every hour.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "hard",
-        "question": "What year was the RoboSapien toy robot released?",
-        "correct_answer": "2004",
-        "incorrect_answers": [
-            "2000",
-            "2001",
-            "2006"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "medium",
-        "question": "In a 1994 CBS interview, Microsoft co-founder Bill Gates performed what unusual trick on camera?",
-        "correct_answer": "Jumping over an office chair",
-        "incorrect_answers": [
-            "Jumping backwards over a desk",
-            "Standing on his head",
-            "Typing on a keyboard during a handstand"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "easy",
-        "question": "Terry Gilliam was an animator that worked with which British comedy group?",
-        "correct_answer": "Monty Python",
-        "incorrect_answers": [
-            "The Goodies&lrm;",
-            "The League of Gentlemen&lrm;",
-            "The Penny Dreadfuls"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "multiple",
-        "difficulty": "hard",
-        "question": "What is the most commonly used noun in the English language?",
-        "correct_answer": "Time",
-        "incorrect_answers": [
-            "Home",
-            "Water",
-            "Man"
-        ]
-    }
-];
-
 testQuestion = [
     {
         "category": "General Knowledge",
@@ -136,16 +17,18 @@ testQuestion = [
 ];
 
 document.body.onload = () => {
-    testData.forEach(question => {
-        let potentialAnswers = shuffleAnswers(getPotentialAnswers(question));
-        // console.log(question.question);
-        // console.log(potentialAnswers);
+    queryTriviaDB("https://opentdb.com/api.php?amount=10&category=9").then(trivia => {
+        trivia.results.forEach(result => {
+            console.log(result.question);
+        });
     });
-    let trivia = queryTriviaDB("https://opentdb.com/api.php?amount=10&category=9");
-    console.log({trivia});
 };
 
-// -----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------- 
+
+function getTriviaUrl() {
+
+}
 
 function getPotentialAnswers(question) {
     let potentialAnswers = [];
@@ -163,16 +46,13 @@ function shuffleAnswers(answers) {
 
 function makeQuiz(triviaData) {
     triviaData.forEach((question, index) => {
+
     });
     drawQuiz(triviaData);
 }
 
 async function queryTriviaDB(url) {
     const response = await fetch(url);
-    return response.json();
-    // fetch(url).then(function (response) {
-    //     response.json().then(function (data) {
-    //         console.log(data.results);
-    //     });
-    // });
+    const trivia = await response.json();
+    return trivia;
 }
